8d8b311ba1039beaaba197008774c721a35d7d31
Natasha <76091986+natashahussain@users.noreply.github.com>
2021-02-08 20:25:45 -0800

    Add files via upload

---
+++ CO1.pg8.py
@@ -0,0 +1,11 @@
+#write a string and  replace ch  with $
+w=str(input("Enter data:"))
+for i in range(0,len(w)):
+    if i==0:
+        print(w[i],end="")
+    else:
+        if w[i] == w[0]:
+            print("$",end="")
+        else:
+            print(w[i],end="")
+
